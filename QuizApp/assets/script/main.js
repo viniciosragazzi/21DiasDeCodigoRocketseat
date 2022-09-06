7d98156d87b02b5850194c8a856e0c49eec35d68
viniciosragazzi <viniciosragazzzi@gmail.com>
2022-09-06 19:01:11 -0300

    Adicionado o desfio Galeria de Fotos

--- QuizApp/assets/script/main.js
+++ QuizApp/assets/script/main.js
@@ -13,19 +13,18 @@ backInicio.addEventListener("click", () => {
   nivelArea.classList.remove("showNivel");
 });
 
+const niveis = document.querySelectorAll(".niveis li"),
+  perguntasArea = document.querySelector(".perguntasArea"),
+  backNiveis = document.querySelector("#backNiveis");
 
-const niveis  = document.querySelectorAll('.niveis li'),
-perguntasArea = document.querySelector('.perguntasArea'),
-backNiveis = document.querySelector('#backNiveis')
-
-niveis.forEach((n)=>{
-    n.addEventListener('click', (e)=>{
-        console.log(        e.path[1].innerText);
-        perguntasArea.classList.add("showNivel");
-    })
-})
-
+niveis.forEach((n) => {
+  n.addEventListener("click", (e) => {
+    console.log(e.path[1].innerText);
+    perguntasArea.classList.add("showNivel");
+  });
+});
 
 backNiveis.addEventListener("click", () => {
-    perguntasArea.classList.remove("showNivel");
-  });
+   perguntasArea.classList.remove("showNivel");
+
+});
