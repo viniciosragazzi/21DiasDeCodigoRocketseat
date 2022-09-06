282f977885f8cf9db4005c56dae8d6792f71c46a
viniciosragazzi <viniciosragazzzi@gmail.com>
2022-09-06 17:58:42 -0300

    Adicionado o desfio Quiz App v1

--- QuizApp/assets/script/main.js
+++ QuizApp/assets/script/main.js
@@ -1,15 +1,31 @@
-const plays = document.querySelectorAll('.svgArea i')
-const nivelArea = document.querySelector('.nivelArea'),
-backInicio = document.querySelector('#backInicio'),
-tema = document.querySelector('.tema')
-plays.forEach((play)=>{
-    play.addEventListener('click', (p)=>{
-        nivelArea.classList.add('showNivel')
-        tema.innerText = p.path[2].children[0].innerText
+const plays = document.querySelectorAll(".svgArea i");
+const nivelArea = document.querySelector(".nivelArea"),
+  backInicio = document.querySelector("#backInicio"),
+  tema = document.querySelector(".tema");
+plays.forEach((play) => {
+  play.addEventListener("click", (p) => {
+    nivelArea.classList.add("showNivel");
+    tema.innerText = p.path[2].children[0].innerText;
+  });
+});
+
+backInicio.addEventListener("click", () => {
+  nivelArea.classList.remove("showNivel");
+});
+
+
+const niveis  = document.querySelectorAll('.niveis li'),
+perguntasArea = document.querySelector('.perguntasArea'),
+backNiveis = document.querySelector('#backNiveis')
+
+niveis.forEach((n)=>{
+    n.addEventListener('click', (e)=>{
+        console.log(        e.path[1].innerText);
+        perguntasArea.classList.add("showNivel");
     })
 })
 
-backInicio.addEventListener('click', ()=>{
-    nivelArea.classList.remove('showNivel')
 
-})
+backNiveis.addEventListener("click", () => {
+    perguntasArea.classList.remove("showNivel");
+  });
